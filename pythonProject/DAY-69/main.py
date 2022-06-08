a2c117eb9cb7bf69f17555e0e845748233fbbc64
eyassaid <58908481+eyassaid@users.noreply.github.com>
2022-06-08 10:59:37 +0400

    Update main.py

--- pythonProject/DAY-69/main.py
+++ pythonProject/DAY-69/main.py
@@ -12,7 +12,7 @@ from flask_gravatar import Gravatar
 from functools import wraps
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY","eyaaas")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app,
